ba5e0df9454719a5037c2abb38b5793b9f518172
projectsm <projectsmstcejorp@gmail.com>
2017-09-01 03:56:20 +0800

    fixed apis for users

--- routes/apis.js
+++ routes/apis.js
@@ -49,5 +49,89 @@ router.get('/users/:id', function(req, res, next) {
 	});	
 });
 
+router.post('/users', function(req, res, next) {
+	// Note: use x-www-form-urlencoded in postman
+	var user = {
+		username: req.body.username,
+		password: req.body.password,
+		type: req.body.type,
+		profile_id : '0'
+	};	
+	
+	pool.query('INSERT INTO users (username, password, type, profile_id) VALUES ($1, md5($2), $3, $4)',[user.username, user.password, user.type, user.profile_id])
+	.then((data) => {
+		res.json({
+			status: 'success',
+			data: {
+				username: user.username,
+				type: user.type
+			},
+			message: 'User has been successfully saved.'
+		});
+	})
+	.catch((e) => {
+		console.error(e.stack);
+		res.json({
+			status: 'failed',
+			data: [],
+			message: 'Unable to save user.'
+		});		
+	});
+});
+
+router.put('/users', function(req, res, next) {
+	// Note: use x-www-form-urlencoded in postman
+	var user = {
+		id: req.body.id,
+		username: req.body.username,
+		password: req.body.password,
+		type: req.body.type,
+		profile_id : req.body.profile_id
+	};	
+	
+	pool.query('UPDATE users SET username=$1, password=md5($2), type=$3, profile_id=$4 WHERE id=$5',[user.username, user.password, user.type, user.profile_id, user.id])
+	.then((data) => {
+		res.json({
+			status: 'success',
+			data: {
+				username: user.username,
+				type: user.type
+			},
+			message: 'User has been successfully updated.'
+		});
+	})
+	.catch((e) => {
+		console.error(e.stack);
+		res.json({
+			status: 'failed',
+			data: [],
+			message: 'Unable to update user.'
+		});		
+	});
+});
+
+router.delete('/users', function(req, res, next) {
+	// Note: use x-www-form-urlencoded in postman
+	var user = {
+		id: req.body.id
+	};	
+	
+	pool.query('DELETE FROM users WHERE id=$1',[user.id])
+	.then((data) => {
+		res.json({
+			status: 'success',
+			data: user,
+			message: 'User has been successfully removed.'
+		});
+	})
+	.catch((e) => {
+		console.error(e.stack);
+		res.json({
+			status: 'failed',
+			data: [],
+			message: 'Unable to remove user.'
+		});		
+	});
+});
 
 module.exports = router;
